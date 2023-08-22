97046363b0f2baf2de4c62664a90671778f7ca96
Gustavo Monarin <gustavomonarin11@gmail.com>
2023-08-22 12:50:46 -0300

    alteracoes da altura e largura manualmente

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,33 +2,46 @@ const config = document.querySelectorAll('[data-config]')
 const caixa = document.querySelector('.caixa-borda')
 const css = document.querySelector('.codigo-css')
 
-let topLeft = 0;
-let bottomLeft = 0;
-let topRight = 0;
-let bottomRight = 0;
+let topo = 100;
+let fundo = 0;
+let direita = 100;
+let esquerda = 0;
+
+gerarCss()
 
 config.forEach(function(elemento){
     elemento.addEventListener('change', function(){
-        if(elemento.classList.value === 'top-left'){
-            caixa.style.borderTopLeftRadius = `${elemento.value}%`;
-            topLeft = elemento.value;
-        } else if (elemento.classList.value === 'botton-left'){
-            caixa.style.borderBottomLeftRadius = `${elemento.value}%`;
-            bottomLeft = elemento.value;
-        } else if (elemento.classList.value === 'top-right'){
-            caixa.style.borderTopRightRadius = `${elemento.value}%`;
-            topRight = elemento.value
-        } else if (elemento.classList.value === 'botton-right'){
-            caixa.style.borderBottomRightRadius = `${elemento.value}%`;
-            bottomRight = elemento.value;
+        if(elemento.classList.value === 'top'){
+            topo = elemento.value;
+        } else if (elemento.classList.value === 'bottom'){
+            fundo = elemento.value;
+        } else if (elemento.classList.value === 'left'){
+            esquerda = elemento.value
+        } else if (elemento.classList.value === 'right'){
+            direita = elemento.value;
         }
-
+        caixa.style.borderRadius = `${topo}% ${100-topo}% ${100-fundo}% ${fundo}% / ${esquerda}% ${direita}% ${100-direita}% ${100-esquerda}%`
         gerarCss();
     })
 })
 
 function gerarCss() {
     css.innerHTML = `.border {<br>
-        &nbsp;&nbsp;&nbsp;&nbsp;border-radius: ${topLeft}% ${bottomLeft}% ${topRight}% ${bottomRight}%;<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;border-radius:${topo}% ${100-topo}% ${100-fundo}% ${fundo}% / ${esquerda}% ${direita}% ${100-direita}% ${100-esquerda}% ;<br>
     }`
 }
+
+const check = document.getElementById('checkbox');
+const tamanho = document.querySelectorAll('.tamanhos')
+
+check.addEventListener('change',function(){
+    if(this.checked) {
+        tamanho.forEach(elemento => {
+            elemento.style.visibility = 'visible'
+        })
+    } else {
+        tamanho.forEach(elemento => {
+            elemento.style.visibility = 'hidden'
+        })
+    }
+})
